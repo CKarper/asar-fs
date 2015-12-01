467565b42fadc26bf4d1764d6241f47bf672d7f5
Christopher Karper <ckarper@tricerat.com>
2015-12-01 00:40:42 -0500

    Reorg project to begin looking presentable

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
 'use strict';
-require('./monkey');
 
-var fs = require('fs');
-
-var test = require('./test');
-var async = require('async');
-var moment = require('moment');
-
-//var props = require('./props/index.json');
-
-console.log(new moment().toString());
-
-test.nike();
-
-fs.readFile('props/index.json', 'utf8', function (err, data) {
-  if (err) { return console.error(err); }
-
-  let props = JSON.parse(data);
-  console.dir(props, { colors: true, depth: 10 });
-});
-
-fs.readFile('scrimp.txt', 'utf8', function (err, data) {
-  if (err) { return console.error(err); }
-
-  console.log(data);
-});
-
-//console.log(props.property);
+module.exports = exports = require('./lib/monkey.js');
